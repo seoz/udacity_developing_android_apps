40c0198cf6c9390d4fb2cd7e8fcb70c35af5ab14
Daniel Juyung Seo <seojuyung2@gmail.com>
2015-08-17 02:19:18 +0900

    Sunshine/Fragment: Rename MainActivityFragment to ForecastFragment.

--- Sunshine/app/src/main/java/com/seoz/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/seoz/sunshine/ForecastFragment.java
@@ -16,11 +16,11 @@ import java.util.List;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ForecastFragment extends Fragment {
 
     ArrayAdapter<String> mForecastAdapter;
 
-    public MainActivityFragment() {
+    public ForecastFragment() {
     }
 
     @Override
